db7b24a2a83a744a10ce473da7b6ce3063ce35d4
James <jy1v07@gmail.com>
2014-02-11 23:22:45 +0000

    attempted to implement slider

--- src/com/jy85/android/MainActivity.java
+++ src/com/jy85/android/MainActivity.java
@@ -6,13 +6,24 @@ package com.jy85.android;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnSeekBarChangeListener{
 
+	private SeekBar bar;
+	private TextView textPercentage;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bar = (SeekBar)findViewById(R.id.seekBar1);
+        bar.setOnSeekBarChangeListener(this);
+        
+        textPercentage = (TextView)findViewById(R.id.textViewPercentage);
+        
     }
 
 
@@ -21,6 +32,21 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int percentage, boolean fromUser) {
+    	textPercentage.setText(percentage + "%");
+    }
+    
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    	
+    }
+    
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    	seekBar.setSecondaryProgress(seekBar.getProgress());
     }
     
 }
